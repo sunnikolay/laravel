72b09d32d3e36b94c50f7228add02c4f3a132527
nikolay <n.stiazhkin@gmail.com>
2023-07-12 21:34:09 +0300

    28 add seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Category::factory(20)->create();
+        $tags = Tag::factory(50)->create();
+        $posts = Post::factory(100)->create();
+        
+        foreach ($posts as $post) {
+            $tagIds = $tags->random(random_int(0, 5))->pluck('id');
+            $post->tags()->attach($tagIds);
+        }
     }
 }
